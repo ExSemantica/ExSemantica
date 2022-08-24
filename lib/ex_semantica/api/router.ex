b64b63ba2ba328338cceda15638871d719cdf7a8
Roland Metivier <metivier.roland@chlorophyt.us>
2022-08-23 22:04:29 -0500

    test cases and router stub added

--- lib/ex_semantica/api/router.ex
+++ lib/ex_semantica/api/router.ex
@@ -1,3 +1,16 @@
+# Copyright 2019-2022 Roland Metivier
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 defmodule ExSemantica.Api.Router do
   @moduledoc """
   Root endpoint router for API access. Forwards to other local endpoints.
@@ -8,15 +21,28 @@ defmodule ExSemantica.Api.Router do
   plug(:match)
   plug(:dispatch)
 
+  @cached_ping Jason.encode!(%{status: :ok})
+  @cached_badrequest Jason.encode!(%{status: :bad_request})
+
+  @doc """
+  Cached /ping response
+  """
+  def cached_ping, do: @cached_ping
+
+  @doc """
+  Cached catchall response for a bad request
+  """
+  def cached_badrequest, do: @cached_badrequest
+
   get "/ping" do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Jason.encode!(%{status: :ok}))
+    |> send_resp(200, @cached_ping)
   end
 
   match _ do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(404, Jason.encode!(%{status: :not_found}))
+    |> send_resp(400, @cached_badrequest)
   end
 end
--- test/api/router_test.exs
+++ test/api/router_test.exs
@@ -1,4 +1,37 @@
+# Copyright 2019-2022 Roland Metivier
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 defmodule ExSemanticaTest.Api.Router do
   use ExUnit.Case
+  use Plug.Test
   doctest ExSemantica.Api.Router
+  @opts ExSemantica.Api.Router.init([])
+
+  test "status check returns ok" do
+    conn = conn(:get, "/ping")
+    conn = conn |> ExSemantica.Api.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == ExSemantica.Api.Router.cached_ping()
+  end
+
+  test "catchall query returns indication of invalid response" do
+    conn = conn(:get, "/invalid")
+    conn = conn |> ExSemantica.Api.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 400
+    assert conn.resp_body == ExSemantica.Api.Router.cached_badrequest()
+  end
 end
