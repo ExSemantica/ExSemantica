ee52066797ee7440cbbff5678637d3de96320efe
Roland Metivier <metivier.roland@chlorophyt.us>
2025-02-12 20:02:45 -0600

    trying diff way of getting libcluster

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :logger, level: :info
 config :libcluster,
   topologies: [
     exsemantica: [
-      strategy: Elixir.Cluster.Strategy.LocalEpmd
+      strategy: Elixir.Cluster.Strategy.Gossip
     ]
   ]
 
