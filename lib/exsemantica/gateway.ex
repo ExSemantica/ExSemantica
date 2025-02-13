ad90275ff7cbfd3393902ab43afb289e6fd306f2
Roland Metivier <metivier.roland@chlorophyt.us>
2025-02-12 20:44:10 -0600

    try handle_cast

--- lib/exsemantica/gateway.ex
+++ lib/exsemantica/gateway.ex
@@ -11,7 +11,7 @@ defmodule Exsemantica.Gateway do
   Starts the Gateway server
   """
   def start_link(_init_args) do
-    GenServer.start_link(__MODULE__, [], name: {:global, __MODULE__})
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   # ===========================================================================
@@ -21,7 +21,7 @@ defmodule Exsemantica.Gateway do
   end
 
   @impl true
-  def handle_info({:user_info, other_node, username, password}, state) do
+  def handle_cast({:user_info, other, username, password}, state) do
     user_data =
       Exsemantica.Repo.one(
         from u in Exsemantica.Repo.User, where: ilike(u.username, ^username), select: u
@@ -29,21 +29,21 @@ defmodule Exsemantica.Gateway do
 
     case user_data do
       nil ->
-        send(other_node, {__MODULE__, self(), {:user_info, {:error, :no_such_item}}})
+        send(other, {__MODULE__, Node.self(), {:user_info, {:error, :no_such_item}}})
 
       %Exsemantica.Repo.User{username: true_username, password: hash} ->
         if Argon2.verify_pass(password, hash) do
           send(
-            other_node,
-            {__MODULE__, self(),
+            other,
+            {__MODULE__, Node.self(),
              {:user_info, {:ok, %{username: true_username}}}}
           )
         else
           Argon2.no_user_verify()
 
           send(
-            other_node,
-            {__MODULE__, self(), {:user_info, {:error, :authentication_failed}}}
+            other,
+            {__MODULE__, Node.self(), {:user_info, {:error, :authentication_failed}}}
           )
         end
     end
@@ -52,7 +52,7 @@ defmodule Exsemantica.Gateway do
   end
 
   @impl true
-  def handle_info({:aggregate_info, other_node, aggregate}, state) do
+  def handle_cast({:aggregate_info, other, aggregate}, state) do
     aggregate_data =
       Exsemantica.Repo.one(
         from a in Exsemantica.Repo.Aggregate, where: ilike(a.name, ^aggregate), select: a
@@ -60,7 +60,7 @@ defmodule Exsemantica.Gateway do
 
     case aggregate_data do
       nil ->
-        send(other_node, {__MODULE__, self(), {:aggregate_info, {:error, :no_such_item}}})
+        send(other, {__MODULE__, Node.self(), {:aggregate_info, {:error, :no_such_item}}})
 
       %Exsemantica.Repo.Aggregate{
         name: true_aggregate,
@@ -68,8 +68,8 @@ defmodule Exsemantica.Gateway do
         inserted_at: inserted_at
       } ->
         send(
-          other_node,
-          {__MODULE__, self(),
+          other,
+          {__MODULE__, Node.self(),
            {:aggregate_info,
             {:ok,
              %{aggregate: true_aggregate, description: description, inserted_at: inserted_at}}}}
@@ -80,8 +80,8 @@ defmodule Exsemantica.Gateway do
   end
 
   @impl true
-  def handle_info({:ping, other_node}, state) do
-    send(other_node, {__MODULE__, self(), :pong})
+  def handle_cast({:ping, other}, state) do
+    send(other, {__MODULE__, Node.self(), :pong})
 
     {:noreply, state}
   end
